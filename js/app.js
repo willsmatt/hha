5366d51d422eebfb263e9d97c707f840e62ed219
Matthew Wills <matt@culture22.com>
2018-01-19 16:25:00 -0600

    addition of filter and careers

--- js/app.js
+++ js/app.js
@@ -1,4 +1,15 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+  // Prepare BackgroundCheck
+  BackgroundCheck.init({
+    targets: '.page-title',
+    images: '.page-header',
+    debug:true
+  });
+});
+
 $(document).foundation();
+
 
 var stickySidebar = $('.sticky');
 
@@ -44,15 +55,27 @@ $(window).resize(function () {
 
 
 $(document).ready(function(){
+
+
+
   $('.hha-slider').slick({
     //setting-name: setting-value
     dots: true,
     autoplay:true,
     autoplaySpeed:6000,
      speed: 1300,
+     arrows: false,
 
   //fade: true,
   //cssEase: 'linear'
+});
+var $status = $('.slide-count');
+var $slickElement = $('.gallery-slider');
+
+$slickElement.on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
+    //currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
+    var i = (currentSlide ? currentSlide : 0) + 1;
+    $status.text(i + '/' + slick.slideCount);
 });
 $('.gallery-slider').slick({
   //setting-name: setting-value
@@ -61,6 +84,21 @@ $('.gallery-slider').slick({
 //fade: true,
 //cssEase: 'linear'
 });
+$('.gallery-slider').slick('setPosition');
+
+$('.gallery').on('click', function (e) {
+    $('.gallery-slider').resize();
+});
+$('[data-reveal]').on('open.zf.reveal', function() {
+	var modal = $(this);
+  $('.gallery-slider').resize();
+	console.log(modal);
+});
+
+
+
+
+
 
   //FULL SCREEN VIDEO
   vpw = window.innerWidth;
